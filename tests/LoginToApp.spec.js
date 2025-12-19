fb14dc9517c64c3048850e2e2dc667cc8629361c
ARPIT POKHAREL <arpitpokharel2@gmail.com>
2025-12-19 20:52:03 +0545

    refactor: remove unused login page and related tests

--- tests/LoginToApp.spec.js
+++
@@ -1,8 +0,0 @@
-import { test, expect } from '@playwright/test';
-import { LoginPage } from '../Templates/LoginPage';
-
-test('login to application', async ({ page }) => {
-    const loginPage = new LoginPage(page);
-    await loginPage.goToLoginPage();
-    await loginPage.login();
-});
